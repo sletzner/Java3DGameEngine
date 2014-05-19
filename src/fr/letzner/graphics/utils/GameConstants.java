bf57f8de4337aeec3b6a704fd509455c8caf55d1
Stefan Letzner <stefanet@free.fr>
2014-05-19 22:30:42 +0200

    Ajout fichiers

---
+++ src/fr/letzner/graphics/utils/GameConstants.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package fr.letzner.graphics.utils;
+
+/**
+ * @author stefan
+ *
+ */
+public class GameConstants {
+	/**
+	 * Vitesse de défilement
+	 */
+	public static float VITESSE = 0.1f;
+	public static String TITLE = "Paysage 3D - Generator"; // window's title
+	public static int WINDOW_WIDTH = 800; // width of the drawable
+	public static int WINDOW_HEIGHT = 600; // height of the drawable
+	public static int FPS = 60; // animator's target frames per second
+	public static String PAYSAGE_IMAGE_PATH = "/home/stefan/Dev/Ressources/TerrainFractal.gif";
+	public static float COEFFICIENT_REDUCTEUR = 10.0f;
+	public static float LARGEUR_CARRE = 1.0f;
+}
